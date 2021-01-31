d1b910684115a779df6bad75b613c7817a9bbb23
Ronald Partridge <rond@ronald-douglas.com>
2021-01-31 01:41:06 -0500

    Stashing date/time timezone conversion progress...

--- cycle_machine/schedule/market_feed/finnhub.py
+++ cycle_machine/schedule/market_feed/finnhub.py
@@ -1,31 +1,14 @@
 import json
 from datetime import datetime
+import pytz
+
 
 import httpx
 from cycle_machine.brain.delta import DeltaSolutionConfig
 from cycle_machine.config import FINNHUB_AUTH_TOKEN
 from cycle_machine.brain.series import Bar
+from pytz import reference
 
-
-# from cycle_machine.schedule.market_feed import MarketFeed
-
-"""
-class OandaMarketFeed(MarketFeed, ABC):
-    def __init__(self, delta_solution_config: DeltaSolutionConfig):
-        # super().__init__(delta_solution_config.symbol)
-        self.account_id = OANDA_ACCOUNT
-        self.auth_token = OANDA_AUTH_TOKEN
-        self.headers = {'Authorization': 'Bearer 5e7e97815705362bc760b2c392cc1b2b-b505d4306b2f9a6c8c3b354ee8b423e9'}
-
-    def get_bars_for(self, period: int):
-        oanda_url_and_rest_prefix = "https://api-fxpractice.oanda.com/v3/accounts/101-002-2782537-002" \
-                                    "/pricing?instruments=XAU_USD"
-        r = httpx.get(oanda_url_and_rest_prefix, headers=self.headers)
-        json_data = json.loads(r.content)
-
-        print("z")
-        pass
-"""
 
 class FinnHubMarketFeed():
     def __init__(self, delta_solution_config: DeltaSolutionConfig):
@@ -48,13 +31,27 @@ class FinnHubMarketFeed():
 
 
     @staticmethod
+    def _finnmhub_response_reduce_to_higher_period(finnhub_response: dict, period: int):
+        return finnhub_response
+        pass
+
+    @staticmethod
     def _finnhub_response_to_bar_list(finnhub_response: dict) -> [Bar]:
         open_list = finnhub_response['o']
         bar_list = []
 
+        """
+        timezone = pytz.timezone("America/Los_Angeles")
+        d_aware = timezone.localize(d)
+        """
+
         for i in range(0, len(open_list)):
+            two_hours = datetime.timedelta(hours=2)
+            bar_date_time = datetime.fromtimestamp(finnhub_response['t'][i]),
+
+
             b = Bar(
-                datetime.fromtimestamp(finnhub_response['t'][i]),
+
                 finnhub_response['h'][i],
                 finnhub_response['l'][i],
                 finnhub_response['o'][i],
@@ -75,6 +72,22 @@ class FinnHubMarketFeed():
             # TODO: YEAH YEAH!!!
             
         """
+        my_timestamp = datetime.fromtimestamp(1611180000)
+
+        # create both timezone objects
+        local_timezone_str = reference.LocalTimezone().tzname(datetime.now())
+        old_timezone = pytz.timezone("America/New_York")
+        new_timezone = pytz.timezone("Israel")
+
+        # tzlocal()).tzname()
+
+        # two-step process
+
+        localized_timestamp = old_timezone.localize(my_timestamp)
+        new_timezone_timestamp = localized_timestamp.astimezone(new_timezone)
+
+        print("z")
+
         resolution = self.__get_resolution(period)
 
         start_time.timestamp()
@@ -83,11 +96,11 @@ class FinnHubMarketFeed():
         to_date = 1611705600
 
         bar_url = self._finnHubUrl + "/forex/candle?symbol=FXPRO:41&resolution=%s&from=%d&to=%d" \
-                  % (resolution, from_date, to_date)
+                  % (resolution, start_time.timestamp(), to_date)
 
         r = httpx.get(bar_url, headers=self.headers)
         json_data = json.loads(r.content)
-
+        json_data = self._finnmhub_response_reduce_to_higher_period(json_data, period)
         bar_list = self._finnhub_response_to_bar_list(json_data)
 
         print("z")
